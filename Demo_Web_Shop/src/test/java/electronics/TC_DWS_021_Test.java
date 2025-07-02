6cfe82977d0949eff04c40ee2daae08d774291e7
Admin <Admin@DESKTOP-FFALLFF>
2025-07-02 16:12:34 +0530

    TC_DWS_021_Test  updated

---
+++ Demo_Web_Shop/src/test/java/electronics/TC_DWS_021_Test.java
@@ -0,0 +1,15 @@
+package electronics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_DWS_021_Test {
+	@Test
+	public void clickONCameraPhoto() {
+		WebDriver driver=new ChromeDriver();
+		
+	}
+	
+
+}
